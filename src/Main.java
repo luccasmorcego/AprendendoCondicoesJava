b4af13ca2a22492951ef560572f93795b3f29523
leogottardo1 <leonardo.28@edu.unipar.br>
2024-08-21 20:52:21 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Hello world!"); }
-    }
+    public static void main(String[] args) {
+
+        Integer idadePessoa = 18;
+        String nomePessoa = "Lucas";
+
+        /*
+        Operadores Logicos
+        * >   Maior
+        * >=  Maior igual
+        * <   Menor
+        * <=  Menor igual
+        * ==  é igual
+        * !=  é diferente
+        * equal compara Textos
+        *
+
+
+         */
+
+        if(idadePessoa > 21) {
+            System.out.println("é maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
+        }
+
+
+        }

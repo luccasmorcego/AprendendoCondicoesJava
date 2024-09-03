6283422699ec78945c69e89d714996a4eb5e3a31
luccasmorcego <gamerlucks248@gmail.com>
2024-09-02 22:46:42 -0300

    save

---
+++ src/Reprovacao.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Reprovacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite sua média final");
+        int nota = scanner.nextInt();
+
+        if (nota >= 7) ;
+        System.out.println("Aprovado");
+    }{ nota >= 5 && nota <7);
+        System.out.println("Recuperação");
+
+       else (nota << 5);
+        System.out.println("Reprovado");
+    }
+}
